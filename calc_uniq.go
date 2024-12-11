097b11a2d2d2dd6bd006b465fe70cce551775aab
Boris Nijikovsky <borisnijikovsky@gmail.com>
2024-12-10 21:33:04 -0500

    implement concurrency

--- calc_uniq.go
+++ calc_uniq.go
@@ -7,64 +7,93 @@ import (
 	"math"
 	"math/bits"
 	"os"
+	"runtime"
+)
+
+var (
+	exp32 = math.Pow(2, 32)
 )
 
 type HyperLogLog struct {
-	registers     []int
-	registerIndex uint
-	b             uint
+	registers            []int
+	totalRegisters       uint    // m - number of registers from Flajolet et al. 2007
+	m                    float64 // float value of total registers (same as m above but decimal)
+	bitsForRegisterIndex uint    // precision (typically between 4 and 16)
+	biasCorrectionConst  float64 // alpha_m from Flajolet et al. 2007
+	valueBitCount        uint    // number of bits after chopping off the number of bitsForRegisterIndex aka register width
 }
 
-func (h HyperLogLog) Add(data []byte) HyperLogLog {
-	x := createHash(data)
-	k := 32 - h.b // first b bits
-	r := leftmostActiveBit(x << h.b)
-	j := x >> k
-
-	if r > h.registers[j] {
-		h.registers[j] = r
+func NewHyperLogLog(bitsForRegisterIndex uint) HyperLogLog {
+	totalRegisters := uint(1 << bitsForRegisterIndex) // Calculate total registers as 2^bitsForRegisterIndex
+	var biasCorrectionConstant float64
+	switch totalRegisters {
+	case 16:
+		biasCorrectionConstant = 0.673
+	case 32:
+		biasCorrectionConstant = 0.697
+	case 64:
+		biasCorrectionConstant = 0.709
+	default:
+		biasCorrectionConstant = 0.7213 / (1.0 + 1.079/float64(totalRegisters))
 	}
-	return h
+	alpha := biasCorrectionConstant
+
+	return HyperLogLog{
+		registers:            make([]int, totalRegisters),
+		totalRegisters:       totalRegisters,
+		m:                    float64(totalRegisters),
+		bitsForRegisterIndex: bitsForRegisterIndex,
+		biasCorrectionConst:  alpha,
+		valueBitCount:        32 - bitsForRegisterIndex,
+	}
 }
 
-func (h HyperLogLog) Count() uint64 {
+func (thisHll HyperLogLog) Add(data []byte) {
+	hashedData := createHash(data)
+	leadingZeros := bits.LeadingZeros32(hashedData << thisHll.bitsForRegisterIndex)
+	firstSetBitIndex := 1 + leadingZeros
+	registerIndex := hashedData >> thisHll.valueBitCount
+
+	if firstSetBitIndex > thisHll.registers[registerIndex] {
+		thisHll.registers[registerIndex] = firstSetBitIndex
+	}
+}
+
+func (thisHll HyperLogLog) Count() uint64 {
 	sum := 0.0
-	m := float64(h.registerIndex)
-	for _, v := range h.registers {
-		sum += math.Pow(math.Pow(2, float64(v)), -1)
+	for _, registerValue := range thisHll.registers {
+		sum += math.Pow(math.Pow(2, float64(registerValue)), -1)
 	}
-	estimate := 0.79402 * m * m / sum
-	// Handle small estimates
-	if estimate <= 2.5*float64(h.registerIndex) {
+	Z := 1.0 / sum // the harmonic mean of the register values aka "indicator" function
+	estimate := thisHll.biasCorrectionConst * thisHll.m * thisHll.m * Z
+	if estimate <= 5.0/2.0*float64(thisHll.totalRegisters) {
 		// Small range correction
 		zeroCount := 0
-		for _, b := range h.registers {
-			if b == 0 {
+		for _, registerValue := range thisHll.registers {
+			if registerValue == 0 {
 				zeroCount++
 			}
 		}
 		if zeroCount > 0 {
-			estimate = float64(h.registerIndex) * math.Log(float64(h.registerIndex)/float64(zeroCount))
+			estimate = float64(thisHll.totalRegisters) * math.Log(float64(thisHll.totalRegisters)/float64(zeroCount))
 		}
+	} else if estimate > 1.0/30.0*exp32 {
+		// Large range correction
+		estimate = -exp32 * math.Log(1-estimate/exp32)
 	}
 	return uint64(estimate)
 }
 
-// Merge merges another HyperLogLog into this one. The number of registers in each must be the same.
-func (h HyperLogLog) Merge(h2 HyperLogLog) error {
-	if h.registerIndex != h2.registerIndex {
-		return fmt.Errorf("number of registers doesn't match: %d != %d", h.registerIndex, h2.registerIndex)
+func (thisHll HyperLogLog) Merge(anotherHll HyperLogLog) error {
+	if thisHll.totalRegisters != anotherHll.totalRegisters {
+		return fmt.Errorf("number of registers doesn't match: %d != %d", thisHll.totalRegisters, anotherHll.totalRegisters)
 	}
-	for j, r := range h2.registers {
-		if r > h.registers[j] {
-			h.registers[j] = r
+	for i, registerValue := range anotherHll.registers {
+		if registerValue > thisHll.registers[i] {
+			thisHll.registers[i] = registerValue
 		}
 	}
 	return nil
-}
-
-func leftmostActiveBit(x uint32) int {
-	return 1 + bits.LeadingZeros32(x)
 }
 
 // create a 32-bit hash
@@ -79,47 +108,108 @@ func createHash(stream []byte) uint32 {
 	return sum
 }
 
+func processFileConcurrently(filePath string, precision uint) (*HyperLogLog, error) {
+	// Get the number of CPU cores
+	numCores := runtime.NumCPU()
+
+	// Get the file size
+	fileInfo, err := os.Stat(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("error getting file info: %v", err)
+	}
+	fileSize := fileInfo.Size()
+
+	// Calculate the size of each part
+	partSize := fileSize / int64(numCores)
+
+	// Create a channel to collect partial HyperLogLog results
+	results := make(chan *HyperLogLog, numCores)
+	errors := make(chan error, numCores)
+
+	// Spawn worker goroutines
+	for i := 0; i < numCores; i++ {
+		startOffset := int64(i) * partSize
+		var endOffset int64
+		if i == numCores-1 {
+			endOffset = fileSize // Last part takes the remainder
+		} else {
+			endOffset = startOffset + partSize
+		}
+
+		go func(start, end int64) {
+			hll := NewHyperLogLog(precision)
+
+			// Open the file and seek to the start offset
+			partFile, err := os.Open(filePath)
+			if err != nil {
+				errors <- fmt.Errorf("error opening file: %v", err)
+				return
+			}
+			defer func(partFile *os.File) {
+				err := partFile.Close()
+				if err != nil {
+
+				}
+			}(partFile)
+
+			_, err = partFile.Seek(start, 0)
+			if err != nil {
+				errors <- fmt.Errorf("error seeking file: %v", err)
+				return
+			}
+
+			// Process the part line by line
+			reader := bufio.NewReader(partFile)
+			currentOffset := start
+			for currentOffset < end {
+				line, err := reader.ReadBytes('\n')
+				currentOffset += int64(len(line))
+				if err != nil {
+					if err.Error() == "EOF" {
+						break
+					}
+					errors <- fmt.Errorf("error reading file: %v", err)
+					return
+				}
+				hll.Add(line)
+			}
+
+			results <- &hll
+		}(startOffset, endOffset)
+	}
+
+	// Aggregate results
+	finalHLL := NewHyperLogLog(precision)
+	for i := 0; i < numCores; i++ {
+		select {
+		case hll := <-results:
+			err := finalHLL.Merge(*hll)
+			if err != nil {
+				return nil, fmt.Errorf("error merging HyperLogLogs: %v", err)
+			}
+		case err := <-errors:
+			return nil, err
+		}
+	}
+
+	return &finalHLL, nil
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Usage: go run main.go <file_path>")
 		return
 	}
 
-	// Open the file containing IP addresses
-	filePath := os.Args[1]
-	file, err := os.Open(filePath)
+	precision := uint(16)
+
+	// Process the file concurrently
+	hll, err := processFileConcurrently(os.Args[1], precision)
 	if err != nil {
-		fmt.Printf("Error opening file: %v\n", err)
-		return
-	}
-	defer func(file *os.File) {
-		err := file.Close()
-		if err != nil {
-
-		}
-	}(file)
-
-	//hll := NewHyperLogLog(1 << 16)
-	var m uint = 1 << 16
-	hll := HyperLogLog{
-		registers:     make([]int, m),
-		registerIndex: m,
-		b:             uint(math.Ceil(math.Log2(float64(1 << 16)))),
-	} // Initialize HyperLogLog with 65536 registers (2^16)
-
-	// Read IP addresses from the file
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		ip := scanner.Text()
-		hll = hll.Add([]byte(ip)) // Add each IP address to HyperLogLog
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Error reading file: %v\n", err)
+		fmt.Printf("Error: %v\n", err)
 		return
 	}
 
-	// Estimate the number of unique addresses
-	uniqueCount := hll.Count()
-	fmt.Printf("Estimated number of unique IPv4 addresses: %d\n", uniqueCount)
+	// Print the estimated count of unique addresses
+	fmt.Printf("Estimated number of unique IPv4 addresses: %d\n", hll.Count())
 }
